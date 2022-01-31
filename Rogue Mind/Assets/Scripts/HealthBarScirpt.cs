e8762a3c60850a8b9969ca9d9ed03c46dc2a227b
radutatar <radu.tatar@outlook.com>
2022-01-31 22:14:22 +0000

    UI set for player on screen

--- Rogue Mind/Assets/Scripts/HealthBarScirpt.cs
+++ Rogue Mind/Assets/Scripts/HealthBarScirpt.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthBarScirpt : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;//the player
-
+	private TextMeshPro playerText;//the text indicating the health
     public Slider slider;// the healthbar itself
 
 	private void Start()
 	{
-		
+		playerText = GetComponentInChildren<TextMeshPro>();
 		player = GameObject.FindGameObjectWithTag("Player");//find the player	
 		//set the max health
 	}
@@ -20,10 +21,12 @@ public class HealthBarScirpt : MonoBehaviour
 	{
 		slider.maxValue = health;
         slider.value = health;
+		playerText.text = health.ToString();
 	}
 
     public void SetHealth(float health)//sets current slider value
 	{
 		slider.value = health;
+		playerText.text = health.ToString();
 	}
 }
