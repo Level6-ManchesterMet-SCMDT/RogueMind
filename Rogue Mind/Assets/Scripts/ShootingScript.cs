51fbc7db51cc1d85d961c001b277f4fe564ecc12
Sam Myatt <80410716+Samuel-Myatt@users.noreply.github.com>
2022-02-02 16:02:23 +0000

    Reloading gun
    Gun reloads with R press also reloads when shot with no ammo

--- Rogue Mind/Assets/Scripts/ShootingScript.cs
+++ Rogue Mind/Assets/Scripts/ShootingScript.cs
@@ -9,26 +9,47 @@ public class ShootingScript : MonoBehaviour
     public GameObject bulletPrefab;//the prefab of the bullet being shot
 
     public float bulletForce = 20f;//the force at which bullets are shot
+    public int currentBullets;
+    public int maxBullets;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentBullets = maxBullets;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))//if left mouse click
+        if (Input.GetMouseButtonDown(0))//if left mouse click
         {
-            Shoot();//Shoot
-		}
-    }
+            if (currentBullets > 0)
+            {
+                Shoot();//Shoot
+                currentBullets--;
+            }
+            else
+            {
+                StartCoroutine(Reload());
+            }
 
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+    }
     void Shoot()
 	{
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);//Create a bullet from the prefab
         Rigidbody2D rigidBody = bullet.GetComponent<Rigidbody2D>();//save it's rigidBody
         rigidBody.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);//add a force based on the bulletForce Variable 
 	}
+
+    private IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1);//pause
+        currentBullets = maxBullets;
+    }
 }
