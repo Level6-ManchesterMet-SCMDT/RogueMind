f2ad95b0151a467fd5f88d513217518acaebbc2c
Charlie-Booth <charliebtf@outlook.com>
2022-02-10 16:44:36 +0000

    Exit floor now works as intended

--- Rogue Mind/Assets/Scripts/Dungeon Generation/ExitFloor.cs
+++ Rogue Mind/Assets/Scripts/Dungeon Generation/ExitFloor.cs
@@ -5,27 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class ExitFloor : MonoBehaviour
 {
-    public Animator transition;
-    public int floorChange;
+    public Animator transition;// gets the transition animation
+    public int floorChange; // how many floors do you need to go back
     public void Start()
     {
-        transition = GameObject.Find("ScreenWipe").gameObject.GetComponent<Animator>();
+        transition = GameObject.Find("ScreenWipe").gameObject.GetComponent<Animator>();// finds the screen wipe object
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Debug.Log("leaving to main menu");
-           StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - floorChange));
+           StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex - floorChange));// changes the scene
 
         }
     }
     IEnumerator LoadLevel(int levelIndex)
     {
         transition.SetTrigger("Start");
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(1.0f);// waits for the transition animation to finish
 
 
-        SceneManager.LoadScene(levelIndex);
+        SceneManager.LoadScene(levelIndex);// loads the level
     }
 }
