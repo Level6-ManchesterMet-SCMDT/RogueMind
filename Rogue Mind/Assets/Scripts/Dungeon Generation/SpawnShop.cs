4434c9abe9582601b2206ac89c3c1046551cd27e
Charlie-Booth <charliebtf@outlook.com>
2022-02-22 12:13:19 +0000

    Camera now follows into shop room

--- Rogue Mind/Assets/Scripts/Dungeon Generation/SpawnShop.cs
+++ Rogue Mind/Assets/Scripts/Dungeon Generation/SpawnShop.cs
@@ -6,15 +6,19 @@ using Cinemachine;
 public class SpawnShop : MonoBehaviour
 {
     public Transform spawnPoint;// the shopkeepers spawnpoint
-    public GameObject shopRoomTileset; // used to activate the new tileset on a shop room
+    //public GameObject shopRoomTileset; // used to activate the new tileset on a shop room
     public GameObject shopKeeper;// holds the shopkeeper
     public bool spawned;// checks to see if the shopkeeper has been spawned, stops repeated shopkeepers spawning on entry
     Transform camera;
-    Transform cinemachineCam;
+    public Transform cinemachineCam;
     
     private void Start()
     {
         camera = GameObject.FindGameObjectWithTag("MainCamera").transform;// finds the main camera
+        
+    }
+    private void Update()
+    {
         cinemachineCam = GameObject.FindGameObjectWithTag("Cinemachine").transform;
     }
 
@@ -22,14 +26,15 @@ public class SpawnShop : MonoBehaviour
     {
         if (other.CompareTag("Player") && !spawned)
         {
-            shopRoomTileset.SetActive(true);// sets the tileset to active
+            //shopRoomTileset.SetActive(true);// sets the tileset to active
             Instantiate(shopKeeper, spawnPoint.position, spawnPoint.rotation);// activates the shopkeeper and his ai
             spawned = true;
         }
         if (other.CompareTag("Player"))
         {
-            camera.GetComponent<CameraScript>().target = this.gameObject.transform;// changes the cameras position
             cinemachineCam.GetComponent<CinemachineVirtualCamera>().Follow = this.gameObject.transform;
+            camera.GetComponent<CameraScript>().target = this.gameObject.transform;// changes the cameras position
+            
         }
     }
 }
